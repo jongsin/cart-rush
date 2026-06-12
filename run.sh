733722e07bf1eb539ec7fe660d9e8cad3e6cbe6c
Toryworks <dev.toryworks@gmail.com>
2026-06-12 16:56:40 +0900

    run.sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+PORTAL_API_BASE=https://games.hometown.co.kr \
+PORTAL_API_KEY=ygk_05ec6264d568dba3d21e6365154d570ba02ac1877ce5d464bec4844c67f068d1 \
+BASE_PATH=/cart-rush \
+docker compose up -d --build
